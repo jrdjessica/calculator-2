5564563afa2b5993db2d8a676c0d746927284a5d
flanasonic <flanasonic@gmail.com>
2022-08-10 18:55:00 -0400

    While loop, user input, and quit statement

--- calculator.py
+++ calculator.py
@@ -5,3 +5,8 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+    user_input = input('Enter your equation here: ')
+    tokens = user_input.split(' ')
+    if tokens[0] == 'q':
+        break
